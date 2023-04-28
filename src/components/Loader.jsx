c98ab01c65aaa6ea286fc61a9f7bf792a5331cfe
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-28 15:04:18 +0300

    update loader size

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="fixed bg-primary w-full h-screen z-50 flex items-center justify-center">
       <div className="loader">
-        <img src={logo} alt="loader" className="w-10 h-10" />
+        <img src={logo} alt="loader" className="w-10 h-10 sm:w-14 sm:h-14" />
       </div>
     </div>
   );
