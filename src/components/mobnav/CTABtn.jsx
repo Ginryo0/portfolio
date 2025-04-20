98c5a177d85c1206ebef58f8d725a52007f5b1fc
Ahmed Zeid <ginryoix@gmail.com>
2025-04-20 13:26:55 +0200

    Update CTA button styling

--- src/components/mobnav/CTABtn.jsx
+++ src/components/mobnav/CTABtn.jsx
@@ -1,13 +1,16 @@
 const CTABtn = ({ className }) => {
   return (
-    <a
-      className={`cta ${className}`}
-      href="https://www.linkedin.com/in/ahmed-zeid-106841257/"
-      rel="noreferrer noopener"
-      target="_blank"
-    >
-      Hire me
-    </a>
+    <div className={`cta  px-4 py-2 ${className}`} data-text="Hire me">
+      <a
+        className={`inline-block `}
+        href="https://www.linkedin.com/in/ahmed-zeid-106841257/"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Hire me
+      </a>
+      <div className="cta-border"></div>
+    </div>
   );
 };
 export default CTABtn;
