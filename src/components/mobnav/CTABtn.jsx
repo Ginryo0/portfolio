237cd4efd87c5f1f54942a03b051b0919148fc52
Ahmed Zeid <ginryoix@gmail.com>
2025-04-20 20:38:41 +0200

    Update CTA

--- src/components/mobnav/CTABtn.jsx
+++ src/components/mobnav/CTABtn.jsx
@@ -1,6 +1,10 @@
-const CTABtn = ({ className }) => {
+const CTABtn = ({ className, ...props }) => {
   return (
-    <div className={`cta  px-4 py-2 ${className}`} data-text="Hire me">
+    <div
+      className={`cta  px-4 py-2 ${className}`}
+      data-text="Hire me"
+      {...props}
+    >
       <a
         className={`inline-block `}
         href="https://www.linkedin.com/in/ahmed-zeid-106841257/"
