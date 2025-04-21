79ed25b7546662cc06f389afaa9ff462d8032897
Ahmed Zeid <ginryoix@gmail.com>
2025-04-21 07:54:14 +0200

    Optimized Earth canvas

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,17 +1,18 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { Preload, useGLTF } from '@react-three/drei';
 import useMobileMedia from '../../hooks/useMobileMedia';
+import useInView from '../../hooks/useInView';
 import CanvasLoader from '../CanvasLoader';
+import ThrottledAutoRotateControls from './ThrottledAutoRotateControls';
 
 const Earth = () => {
   const earth = useGLTF('./planet/scene.gltf');
   const { isMobile } = useMobileMedia();
-
   return (
     <primitive
       object={earth.scene}
-      scale={isMobile ? 3 : 2}
+      scale={isMobile ? 3 : 1.8}
       position-y={0}
       rotation-y={0}
     ></primitive>
@@ -19,31 +20,31 @@ const Earth = () => {
 };
 
 const EarthCanvas = () => {
+  //Render only when 10% is visible for performance
+  const [ref, inView] = useInView({ threshold: 0.1 });
   return (
-    <Canvas
-      shadows
-      frameloop="demand"
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-      }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={-3}
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Earth />
-        <Preload all />
-      </Suspense>
-    </Canvas>
+    <div ref={ref} className="w-full h-full">
+      {inView && (
+        <Canvas
+          // shadows
+          frameloop="demand"
+          dpr={[1, 1.2]}
+          gl={{ antialias: true }}
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [-4, 3, 6],
+          }}
+        >
+          <Suspense fallback={<CanvasLoader />}>
+            <ThrottledAutoRotateControls autoRotate />
+            <Earth />
+            <Preload all />
+          </Suspense>
+        </Canvas>
+      )}
+    </div>
   );
 };
 
