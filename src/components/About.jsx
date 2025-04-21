27b1489b59c8cfa9f8f7d13c1d9ad925c567e73d
Ahmed Zeid <ginryoix@gmail.com>
2025-04-21 15:43:33 +0200

    Update About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ import SectionWrapper from './hoc/SectionWrapper';
 
 const About = () => {
   return (
-    <d>
-      <motion.div variants={textVariant()}>
+    <>
+      <motion.div variants={textVariant()} className="md:col-span-2">
         {/* <img src={astro} style={{ opacity: '0.5', width: '20%' }} /> */}
         <p className={`${styles.sectionSubText} `}>Overview</p>
         <h2 className={styles.sectionHeadText}>About</h2>
@@ -19,21 +19,21 @@ const About = () => {
         <p className={styles.sectionText}>
           I’m a <span className="animated-text"> Full-Stack Developer</span>,
           with a couple of years of hands-on experience building{' '}
-          <span className="animated-text"></span>AI-powered tools, SaaS
+          <span className="animated-text">AI-powered Web Apps</span>, SaaS
           platforms, and Chrome extensions. I’m all about blending creativity
           with functionality to deliver intuitive, polished digital products.
         </p>
         <p className={styles.sectionText}>
           Driven by curiosity and a love for emerging tech, I build{' '}
-          <span className="animated-text">interactive tools</span> and platforms
-          that spark engagement and streamline user experience. From chatbot
-          generators to dashboards, my focus is on creating polished,{' '}
+          <span className="animated-text">interactive tools</span> that spark
+          engagement and streamline user experience. From chatbot generators to
+          dashboards, my focus is on creating polished,{' '}
           <span className="animated-text">forward-thinking</span> web apps that
           feel as good as they function.
         </p>
       </motion.div>
-    </d>
+    </>
   );
 };
 
-export default SectionWrapper(About, 'about', ' max-w-xl sm:w-3/4 w-full');
+export default SectionWrapper(About, 'about', 'md:col-span-2');
