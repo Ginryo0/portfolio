0e44e3cfe643ac0a82a3d96e671016ef9f44b299
Ahmed Zeid <ginryoix@gmail.com>
2025-04-22 17:55:04 +0200

    Remove old assets

--- src/assets/index.js
+++
@@ -1,8 +0,0 @@
-import logo from './logo.svg';
-import astro from './astro.png';
-
-import jobify from './projects/jobify.png';
-import appie from './projects/appie.png';
-import breakout from './projects/breakout.png';
-
-export { logo, astro, jobify, appie, breakout };
