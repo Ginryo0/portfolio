c1ba66b60218671169cda2a3f93b64e4d4eab541
Ahmed Zeid <ginryoix@gmail.com>
2025-04-22 08:55:46 +0200

    Update achievements

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -13,36 +13,31 @@ const Achievements = () => {
       <motion.div
         variants={slideIn('right', 'spring', 0.2, 2)}
         transition={{ staggerChildren: 0.5 }}
+        className="flex flex-col flex-wrap justify-center w-full gap-2 p-4 md:items-center text-tertiary"
       >
-        <div className="flex flex-col flex-wrap items-center justify-center gap-2 p-4 text-tertiary">
-          <AchievementCard
-            icon={<Zap size={20} />}
-            value="5+"
-            label="Full Stack Projects"
-          />
-          <AchievementCard
-            icon={<Delivery className={'w-6 h-6'} />}
-            value="2+"
-            label="Years of Experience"
-          />
-          <AchievementCard
-            icon={<Bug size={20} />}
-            value="999+"
-            label="Bugs Squashed"
-          />
-          <AchievementCard
-            icon={<CoffeeIcon size={20} />}
-            value="567+"
-            label="Cups of Coffee"
-          />
-        </div>
+        <AchievementCard
+          icon={<Zap size={20} />}
+          value="5+"
+          label="Full Stack Projects"
+        />
+        <AchievementCard
+          icon={<Delivery className={'w-6 h-6'} />}
+          value="2+"
+          label="Years of Experience"
+        />
+        <AchievementCard
+          icon={<Bug size={20} />}
+          value="999+"
+          label="Bugs Squashed"
+        />
+        <AchievementCard
+          icon={<CoffeeIcon size={20} />}
+          value="567+"
+          label="Cups of Coffee"
+        />
       </motion.div>
     </>
   );
 };
 
-export default SectionWrapper(
-  Achievements,
-  'tech',
-  'w-full text-center h-full flex flex-col items-center justify-center overflow-hidden justify-self-start'
-);
+export default Achievements;
